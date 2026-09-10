654af1f8bd020413b78fcddbc28e82096fb17afd
sejal bahekar <sejalbahekar504@gmail.com>
2026-09-10 09:41:13 +0530

    Update project

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ import InterviewHistory from './pages/InterviewHistory'
 import Pricing from './pages/Pricing'
 import InterviewReport from './pages/InterviewReport'
 
-export const ServerUrl  = "https://interviewiq-2-75hk.onrender.com"
+export const ServerUrl  = " https://interviewiq-stfa.onrender.com"  
 
 function App() {
 
